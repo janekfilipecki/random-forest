78bbdbc2891d3e02a3fc4d6553203fe077a51fb6
MA3CIN <marcin.zet@onet.pl>
2024-01-20 21:44:47 +0100

    data

--- visualize.py
+++ visualize.py
@@ -1,31 +1,38 @@
 import plotly.graph_objects as go
 
-months = ['True','False']
-before = []
-after  = []
+months = ['True','False','lol']
+false = [0,8613861386138614,
+0,9454545454545454,
+0,8253968253968254,
+0,8813559322033897, 1,531]
+true  = [0,8547854785478548,
+0,9454545454545454,
+0,8167539267015707,
+0,8764044943820224, 1,5166
+]
 
 fig = go.Figure()
 fig.add_trace(go.Bar(
     x=months,
-    y=[20, 0.8514851485148515],
+    y=[true[0], false[0]],
     name='Accuracy'))
 fig.add_trace(go.Bar(
     x=months,
-    y=[19, 0.9454545454545454],
+    y=[true[1], false[1]],
     name='Recall'
 ))
 fig.add_trace(go.Bar(
     x=months,
-    y=[20, 0.8125],
+    y=[true[2], false[2]],
     name='Precision'))
 fig.add_trace(go.Bar(
     x=months,
-    y=[19, 14],
+    y=[true[3], false[3]],
     name='F1'
 ))
 fig.add_trace(go.Bar(
     x=months,
-    y=[19, 14],
+    y=[true[4], false[4]],
     name='Time'
 ))
 
